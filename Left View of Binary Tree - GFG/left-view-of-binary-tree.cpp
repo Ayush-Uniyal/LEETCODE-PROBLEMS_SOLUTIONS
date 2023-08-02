32ce9078729f20b9e58637d6ef95b09f463f073a
Ayush Uniyal <89723028+Ayush-Uniyal@users.noreply.github.com>
2023-08-01 23:39:25 -0400

    Added solution - LeetHub

--- Left View of Binary Tree - GFG/left-view-of-binary-tree.cpp
+++ Left View of Binary Tree - GFG/left-view-of-binary-tree.cpp
@@ -128,16 +128,16 @@ struct Node
  */
 
 //Function to return a list containing elements of left view of the binary tree.
-void recursion(Node* root, int level, vector<int>& res);
-vector<int> leftView(Node *root)
-{
-   vector<int>res;
-   recursion(root,0,res);
-   return res;// Your code here
-}
 void recursion(Node *root,int level,vector<int>&res){
     if(root==NULL)  return;
     if(res.size()==level) res.push_back(root->data);
     recursion(root->left,level+1,res);
     recursion(root->right,level+1,res);
 }
+vector<int> leftView(Node *root)
+{
+   vector<int>res;
+   recursion(root,0,res);
+   return res;// Your code here
+}
+
